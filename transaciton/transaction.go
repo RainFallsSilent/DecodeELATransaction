33239302948877fe8c59c864551d004242e0e34d
江泽华 <1092431698@qq.com>
2024-02-06 15:38:25 +0800

    add public key and code to address function

--- transaciton/transaction.go
+++ transaciton/transaction.go
@@ -2,8 +2,9 @@ package transaciton
 
 import (
 	"bytes"
-
+	"encoding/hex"
 	"github.com/elastos/Elastos.ELA/common"
+	"github.com/elastos/Elastos.ELA/core/contract"
 	"github.com/elastos/Elastos.ELA/core/transaction"
 	"github.com/elastos/Elastos.ELA/servers"
 )
@@ -23,4 +24,39 @@ func DecodeRawTransaction(rawTransaction string) (*servers.TransactionInfo, erro
 	}
 
 	return servers.GetTransactionInfo(txn), nil
+}
+
+func PublicKeyToAddress(pubKeyHex string) (string, error) {
+	pubKey, err := common.HexStringToBytes(pubKeyHex)
+	if err != nil {
+		return "", err
+	}
+	programHash, err := contract.PublicKeyToStandardProgramHash(pubKey)
+	if err != nil {
+		return "", err
+	}
+	addr, err := programHash.ToAddress()
+	if err != nil {
+		return "", err
+	}
+
+	return addr, nil
+}
+
+func CodeToAddress(codeStr string) (string, error) {
+	code, err := hex.DecodeString(codeStr)
+	if err != nil {
+		return "", err
+	}
+	codeContract, err := contract.CreateStandardContractByCode(code)
+	if err != nil {
+		return "", err
+	}
+	codeProgramHash := codeContract.ToProgramHash()
+	codeAddr, err := codeProgramHash.ToAddress()
+	if err != nil {
+		return "", err
+	}
+
+	return codeAddr, nil
 }
